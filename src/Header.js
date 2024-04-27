1abc1de94c02a28348987b76052801a96197c412
Kevin Coraza <kevcoraza@gmail.com>
2024-04-27 09:46:59 +0800

    a
    a

---
+++ src/Header.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+function Header() {
+  const currentHour = new Date().getHours();
+  const isNightTime = currentHour >= 18 || currentHour <= 6;
+
+  const backgroundColor = isNightTime ? "bg-gray-800" : "bg-orange-100";
+  const textColor = isNightTime ? "text-white" : "text-black";
+
+  return (
+    <header>
+      <nav>
+        <div
+          className={
+            " bg-smile-pink text-white p-4 flex justify-between items-center"
+          }
+        >
+          <div>
+            {/*logo here */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
+              />
+            </svg>
+          </div>
+          <div className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 30s linear infinite",
+        marquee2: "marquee2 30s linear infinite",
+        marquee3: "marquee3 20s linear infinite",
+        marquee4: "marquee4 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "100%": { transform: "translateX(-100%)" },
+          "0%": { transform: "translateX(%)" },
+        },
+        marquee2: {
+          "100%": { transform: "translateX(%)" },
+          "0%": { transform: "translateX(100%)" },
+        },
+        marquee3: {
+          "100%": { transform: "translateX(%)" },
+          "0%": { transform: "translateX(-100%)" },
+        },
+        marquee4: {
+          "100%": { transform: "translateX(100%)" },
+          "0%": { transform: "translateX(%)" },
+        },
+      },
+      colors: {
+        "smile-pink": "#f376ac",
+      },
+    },
   },
   plugins: [],
-}
-
+};
