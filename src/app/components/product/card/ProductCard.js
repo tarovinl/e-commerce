fa02dd7505724cfe19e81f9e5d5179ee37ef42e5
mcsnette <126247297+mcsnette@users.noreply.github.com>
2024-05-16 16:13:11 +0800

    hover of product items

--- src/app/components/product/card/ProductCard.js
+++ src/app/components/product/card/ProductCard.js
@@ -8,12 +8,14 @@ function ProductCard({ product }) {
         to={`/product/${product.id}`}
         className="text-dark no-underline hover:text-gray-500 text-center font-futurabook"
       >
-        <div className="bg-white max-w-sm mx-auto p-4">
-          <img
-            className="w-full h-96 object-cover mb-4"
-            src={product.image}
-            alt={product.title}
-          />
+        <div className="bg-white max-w-sm mx-auto p-4 w-full relative">
+          <div className="overflow-hidden">
+            <img
+              className="w-full h-96 object-cover mb-4 transition-transform duration-300 hover:scale-105"
+              src={product.image}
+              alt={product.title}
+            />
+          </div>
           <span className="text-gray-600 text-xs capitalize block my-2">
             {product.category}
           </span>
