b28835e9c6115f0c93dbda2c7350b566a45e1d94
daeneerys <johndaniel.ocampo.cics@ust.edu.ph>
2024-05-17 23:53:28 +0800

    ===

--- src/app/components/checkout-form/CompleteForm.js
+++ src/app/components/checkout-form/CompleteForm.js
@@ -3,8 +3,14 @@ import { Button, Modal } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { resetCart } from "../../features/cart/CartSlice";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 
 function CompleteForm() {
+  const form = useRef();
+  const navigate = useNavigate();
+
   const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
 
@@ -16,12 +22,125 @@ function CompleteForm() {
     dispatch(resetCart()); // Dispatch the resetCart action
   };
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_0mxz15r", "template_hr2j6l4", form.current, {
+        publicKey: "FF08ux6H7-xfSF0HJ",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          handleShowModal();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <Fragment>
       <h1>Complete form</h1>
-      <Link to="/completeform">
-        <Button onClick={handleShowModal}>Continue order</Button>
-      </Link>
+      <div>
+        <p>
+          For customs purposes, you must enter the following details in latin
+          characters.
+        </p>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            id="name"
+            placeholder="Name*"
+            name="from_name"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="name"
+            placeholder="Surname*"
+            name="from_surname"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="from_email"
+            //value = {}
+            required
+          />
+          <input
+            type="text"
+            id="country"
+            placeholder="Country*"
+            from="from_country"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="address"
+            placeholder="Address*"
+            name="from_address"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="address-2"
+            placeholder="Address (line 2)*"
+            name="from_address-2"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="post-code"
+            placeholder="Post code*"
+            name="from_post-code"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="region"
+            placeholder="Region*"
+            name="from_region"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="text"
+            id="town"
+            placeholder="Town / City*"
+            name="from_city"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <input
+            type="tel"
+            id="phone"
+            placeholder="Phone Number (09**-***-****)*"
+            pattern="[0-9]{4}-[0-9]{3}-[0-9]{4}"
+            name="from_phone_number"
+            //value = {}
+            //onChange = {}
+            required
+          />
+          <button type="submit">Continue order</button>
+        </form>
+      </div>
 
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
