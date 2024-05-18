1ac35321c5903236ce7f6be0f7c692323256a083
TaculaIT <karl.tacula.cics@ust.edu.ph>
2024-05-18 20:20:21 +0800

    navbar hamburger keme fux

--- src/app/components/header/TopNavbar.js
+++ src/app/components/header/TopNavbar.js
@@ -42,13 +42,13 @@ const TopNavbar = () => {
         </NavLink>
 
         {showSearchBar && <SearchBar initialQuery="" />}
-        <div className="md:hidden ">
+        <div className="md:hidden">
           <button
             onClick={toggleMenu}
-            className="relative focus:outline-none focus:shadow-outline"
+            className="relative focus:outline-none focus:shadow-outline hover:bg-gray-200 p-2 rounded"
           >
             <svg
-              className="h-6 w-6 fill-current"
+              className="h-6 w-6 fill-current text-gray-700 hover:text-gray-900 transition-colors duration-200"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
             >
